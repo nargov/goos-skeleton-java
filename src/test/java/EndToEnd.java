70a9fe2bb2cb9138df448bb7aa58d9878263d184
Nimrod Argov <nimrod@wishkeeper.co>
2019-08-12 14:07:42 +0300

    Add server instructions in the end to end test file

--- src/test/java/EndToEnd.java
+++ src/test/java/EndToEnd.java
@@ -1,5 +1,19 @@
 /**
  * For the tests in this class to work, an Openfire XMPP Server needs to be running in the background.
+ *
+ * You can download the Openfire server here: https://www.igniterealtime.org/downloads/index.jsp
+ *
+ * Make sure to configure the server in the admin console:
+ *
+ * Set up 3 user accounts (username/password):
+ * sniper/sniper
+ * auction_item_54321/auction
+ * auction_item_65432/auction
+ *
+ * In the server settings set it to:
+ * 1) not store offline messages
+ * 2) system name = localhost
+ * 3) resource police = never kick
  */
 
 public class EndToEnd {
